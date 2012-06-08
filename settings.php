9f6059473bea098b380ded11bd6c5fd726bb7525
ToeBee <toby.murray@gmail.com>
2012-06-07 22:28:13 -0400

    Remove HTML from settings file. Wasn't thinking when I added it.

--- settings.php
+++ settings.php
@@ -7,7 +7,7 @@
     global $height;
     global $width;
     #Change this! Just to prevent random people from being able to change your displayed location
-    $secretKey = 'abcdefg';
+    $secretKey = '4fkw8vxp';
     #used to round the lat/lon values so you aren't sharing your *exact* location
     $accuracy = 2; 
     $filePath = '/tmp/location';
@@ -20,9 +20,3 @@
     $height = "500";
     $width = "600";
 ?>
-<html>
-<body>
-  <p>This is just a settings file. Nothing to see here. Move along now.</p>
-  <p>You probably want the <a href="map.php">Map page</a></p>
-</body>
-</html>
