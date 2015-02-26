33da0ce718b382e292564fc84d94e668d5fdc07b
Luc <git-client@lucb1e.com>
2015-02-26 21:01:17 +0100

    Better instructions in README; API key for mapquest (they require it now); input sanitization; added (commented-out) code for an interative map (albeit with no marker); constant-time comparison for secretKey; better documentation of the settings in settings.php; https url for osm.org; checks that $secretKey is set (application will refuse to work with default key); changed includes to requires. In short: slight improvements, mostly to make it easier to deploy.

--- map.php
+++ map.php
@@ -1,5 +1,8 @@
 <?php
-    include "settings.php";
+    require "settings.php";
+	if ($secretKey == 'CHANGEME') {
+		die('This application is not functional yet. See the README how to set it up (hint: edit settings.php).');
+	}
 
     $loc_string = file_get_contents($filePath);
     $info = unserialize($loc_string);
@@ -8,22 +11,35 @@
     $lat = $info['lat'];
     $lon = $info['lon'];
     $staticUrl = 'http://open.mapquestapi.com/staticmap/v4/getmap?';
-    $staticUrl = $staticUrl."size=$width,$height&type=map&imagetype=JPEG";
+    $staticUrl = $staticUrl."size=$width,$height&type=map&imagetype=JPEG&key=$apikey";
     $staticUrl = $staticUrl . "&pois=mcenter,$lat,$lon|&zoom=$zoom&center=$lat,$lon";
-    $mapUrl = 'http://www.openstreetmap.org';
+    $mapUrl = 'https://www.openstreetmap.org';
     $mapUrl = $mapUrl . "?mlat=$lat&mlon=$lon";
 ?>
 
 <html>
 <head>
     <title><?php echo $name?>'s Location</title>
-    <meta http-equiv="refresh" content="600" />
+    <meta http-equiv="refresh" content="60" />
 </head>
 <body>
     <h3><?php echo $name?>'s status as of <?php echo $minutesAgo?> minutes ago:</h3>
     <h3>Speed: <?php echo $info['speed']?> m/s</h3>
     <a href=<?php echo $mapUrl?>><img src="<?php echo $staticUrl?>"></a><br/>
     <p>Click for an interactive map</p>
+	<?php /* Note that you can embed an interactive map using:
+
+		$bbox = 0.01; // Zoom. 0.1 gives an overview, 0.01 is quite zoomed in, 0.001 is street-level.
+		$bboxleft = $lon - $bbox;
+		$bboxbottom = $lat - $bbox;
+		$bboxright = $lon + $bbox;
+		$bboxtop = $lat + $bbox;
+		$bb = urlencode($bboxleft . ',' . $bboxbottom . ',' . $bboxright . ',' . $bboxtop);
+		<iframe src="http://www.openstreetmap.org/export/embed.html?m&bbox=<?php echo $bb; ?>&amp;layer=mapnik" width=800 height=600></iframe>
+
+		The issue is that you cannot place a marker (not that I have found).
+	*/
+	?>
 
 </body>
 </html>
--- settings.php
+++ settings.php
@@ -1,22 +1,25 @@
 <?php
-    global $secretKey;
-    global $filePath;
-    global $name;
-    global $zoom;
-    global $accuracy;
-    global $height;
-    global $width;
-    #Change this! Just to prevent random people from being able to change your displayed location
-    $secretKey = '4fkw8vxp';
-    #used to round the lat/lon values so you aren't sharing your *exact* location
-    $accuracy = 2; 
+    global $secretKey, $filePath, $name, $zoom, $accuracy, $height, $width;
+
+    // This key must be entered in OsmAnd's tracking URL as &key=
+    $secretKey = 'CHANGEME';
+
+	// API key from MapQuest. Get one from developer.mapquest.com
+	$apikey = '';
+
+    // Used to round the lat/lon values so you aren't sharing your *exact* location
+    $accuracy = 2; // in number of decimal positions
+
+	// Where to store your last location
     $filePath = '/tmp/location';
 
-    # Display options
-
+	// Used in page title
     $name = 'Toby';
-    #how far to zoom in on the map
+
+	// Zoom on the map
+	// 8 gives a high overview; 12 gives a more zoomed-in map. Lower or higher values also possible.
     $zoom = "8";
+
     $height = "500";
     $width = "600";
-?>
+
